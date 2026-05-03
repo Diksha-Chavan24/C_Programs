719106b3a20ccd11495f26c44c4cdd2086a51ab7
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-03 13:25:13 +0000

    Add files via upload

---
+++ program20.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEven(int iNo)
+{
+    bool bResult = false;
+
+    if((iNo % 2) == 0)
+    {
+        bResult = true;
+    }
+    else
+    {
+        bResult = false;
+    }
+    return bResult;
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number to  check whether it is even or odd : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckEven(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is Even number\n",iValue);
+    }
+    else
+    {
+        printf("%d is Odd number\n",iValue);
+    }
+
+    return 0;
+}
