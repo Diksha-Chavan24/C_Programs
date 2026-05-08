5399fbf1afe371ec3f20584438983add9588897d
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-08 15:33:53 +0000

    Add files via upload

---
+++ program76.c
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     program76.c
+//  Descreption :   Used to accept number and print * of that frequency
+//  Input :         4
+//  Output :        $ $ $ $
+//  Author :        Diksha Anil Chavan
+//  Date :          18/05/2025
+//
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("$\t");
+    }
+    printf("\n");
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter frequency : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+//Time complexity = O(N)
+//N is natural number
+//Where N >= 0 and N <= infinity
