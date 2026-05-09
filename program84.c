ca03ae2a0f2b768425e49e004ac95bfc8484c068
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-09 17:50:10 +0000

    Add files via upload

---
+++ program84.c
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     program84.c
+//  Input :         4
+//  Output :        -4 -3 -2 -1 0 1 2 3 4
+//  Author :        Diksha Anil Chavan
+//  Date :          18/05/2025
+//
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    for(iCnt = -iNo; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t ",iCnt);
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter frequency : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+//Time complexity = O(N)
+//N is natural number
+//Where N >= 0 and N <= infinity
