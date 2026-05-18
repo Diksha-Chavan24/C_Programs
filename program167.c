f8a8ffdcac5f7e724f8297db24b532f77483e61b
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-18 17:29:06 +0000

    Add files via upload

---
+++ program167.c
@@ -0,0 +1,19 @@
+//By using string library for counting length of string
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s", Arr);
+
+    iRet = strlen(Arr);
+
+    printf("Number of characters are : %d\n", iRet);
+
+    return 0;
+}
