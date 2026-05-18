4a58e3d0e3659f8bc35873a1df2a67f052bef490
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-18 17:28:28 +0000

    Add files via upload

---
+++ program165.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void Display(char str[])
+{
+    int i = 0;          //here we wrote the for loop by using counter
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        printf("%c\n",str[i]);
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s", Arr);
+
+    Display(Arr);
+
+    return 0;
+}
