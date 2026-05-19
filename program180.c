83fac33cfb3fb9266be0d8ce16b4ad2f878ad3a1
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-19 13:19:44 +0000

    Add files via upload

---
+++ program180.c
@@ -0,0 +1,31 @@
+//input : India
+//output : india
+
+#include<stdio.h>
+
+void strlwrX(char *str)
+{
+
+    while(*str != '\0')
+    {
+        if((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + ('a' - 'A'); //address arithematic
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s", Arr);
+
+    strlwrX(Arr);
+
+    printf("Updated string : %s\n",Arr);
+
+    return 0;
+}
