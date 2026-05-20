9afb27bae1b2cb393fe1850f0a7ad8bc06534ad1
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-20 16:00:10 +0000

    Add files via upload

---
+++ program182.c
@@ -0,0 +1,36 @@
+//input : India
+//output : iNDIA
+
+#include<stdio.h>
+
+void strtoggleX(char *str)
+{
+
+    while(*str != '\0')
+    {
+        if((*str >= 'a') && (*str <= 'z'))
+        {
+            *str = *str - 32; 
+
+        }
+        else if((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + 32;
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s", Arr);
+
+    strtoggleX(Arr);
+
+    printf("Updated string : %s\n",Arr);
+
+    return 0;
+}
