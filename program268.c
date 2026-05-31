6b9c8d7295133df35d105e848098603cbe72ad8e
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-05-31 16:45:32 +0000

    Add files via upload

---
+++ program268.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    printf("Number in decimal format : %d\n",iValue);
+    printf("Number in octal format : %o\n",iValue );
+    printf("Number in hexadecimal format : %x\n",iValue);
+
+    return 0;
+}
+
